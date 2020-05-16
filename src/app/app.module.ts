8d68d2b607c0f817170262a6039eb970aab9a614
foramgajera <f.c.gajera.fg@gmail.com>
2020-05-16 18:45:31 +0530

    feedback form and validation added but whe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,9 +11,12 @@ import { MatButtonModule } from '@angular/material/button';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 import { MatDialogModule } from '@angular/material/dialog';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 
 import { DishService } from './services/dish.service';
@@ -58,7 +61,10 @@ import { LoginComponent } from './login/login.component';
     MatCheckboxModule,
     MatFormFieldModule,
     MatInputModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule,
+    MatSelectModule,
+    MatSlideToggleModule
   ],
   entryComponents: [ LoginComponent ],
   providers: [DishService, PromotionService, LeaderService],
